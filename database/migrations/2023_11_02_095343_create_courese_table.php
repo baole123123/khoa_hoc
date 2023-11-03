4781c656f74d52bec12caed29e1035374b3c349b
baole123123 <lechibaovlog@gmail.com>
2023-11-03 08:18:12 +0700

    hoan thanh migration

---
+++ database/migrations/2023_11_02_095343_create_courese_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image');
+            $table->string('status');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('level_id')->constrained('levels');
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
