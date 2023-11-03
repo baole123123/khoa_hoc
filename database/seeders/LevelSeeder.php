a880b3584058d62baaed361e1f3ba5b4be73df55
baole123123 <lechibaovlog@gmail.com>
2023-11-03 09:46:06 +0700

    hoan thanh seeder

---
+++ database/seeders/LevelSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('levels')->insert([
+            [
+                'name' => 'Admin'
+            ],
+        ]);
+    }
+}
